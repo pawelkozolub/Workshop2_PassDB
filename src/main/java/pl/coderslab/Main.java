847237531e256efe7c65cd34a5fb0a42b4ed80b7
pawelkozolub <pkozolub@gmail.com>
2023-05-21 17:13:34 +0200

    Main class file

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,20 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection conn = DbUtil.connect()) {
+            System.out.println("Connected to database workshop2...");
+        } catch (SQLException e) {
+            System.out.println("Exception occurred...");
+            e.printStackTrace();
+        }
+
+        // UserDao methods tested in MainDao class in entity package
     }
 }
